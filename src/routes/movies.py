bc91dc3f6a53c413b0089ddd3dc758c73c9b7741
Andrii Omelchenko <andreyomem@gmail.com>
2025-08-21 18:13:12 +0300

    add movie list

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -7,7 +7,50 @@ from sqlalchemy.orm import joinedload
 from database import get_db, MovieModel
 from database.models import CountryModel, GenreModel, ActorModel, LanguageModel
 
+from schemas.movies import MoviesListResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/", response_model=MoviesListResponseSchema)
+async def list_movies(
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
+        db: AsyncSession = Depends(get_db),
+):
+    total_items = await db.execute(select(func.count(MovieModel.id)))
+    total_items = total_items.scalar()
+
+    if total_items == 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    total_pages = (total_items + per_page - 1) // per_page
+
+    if page > total_pages:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    result = await db.execute(
+        select(MovieModel)
+        .order_by(MovieModel.id.desc())
+        .offset((page - 1) * per_page)
+        .limit(per_page)
+    )
+    movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    prev_page = (
+        f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    )
+    next_page = (
+        f"/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
+    )
+
+    return MoviesListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items,
+    )
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,25 @@
 # Write your code here
+from typing import Optional, List
+
+from pydantic import BaseModel
+from datetime import date
+
+
+class MovieBase(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    overview: Optional[str]
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class MoviesListResponseSchema(BaseModel):
+    movies: List[MovieBase]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
